0e34be9c23dd3d5b44a5625576f02a2bd9e4acd6
Pankaj Kumar <pankaj.ky3007@gmail.com>
2024-03-02 22:59:31 +0530

    some message

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <h1 className='text-center'>Submit Page</h1>
+      <h1 className='text-4xl text-center'>I Didn't understand what the problem statement says in line no 4</h1>
+      
+    </div>
   )
 }
 
